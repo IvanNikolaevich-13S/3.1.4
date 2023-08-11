b1e7bc9451368c933bd865f9aead06cf90f0ff2e
IvanNikolaevich-13S <ivan.nikolaevich.13s@gmail.com>
2023-08-11 19:25:10 +0700

    change import class

--- src/main/java/ru/kata/spring/boot_security/demo/Util/UserValidator.java
+++ src/main/java/ru/kata/spring/boot_security/demo/Util/UserValidator.java
@@ -2,19 +2,20 @@ package ru.kata.spring.boot_security.demo.Util;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 import ru.kata.spring.boot_security.demo.models.User;
-import ru.kata.spring.boot_security.demo.service.UserValidService;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
 
 @Component
 public class UserValidator implements Validator {
-    public final UserValidService userValidService;
+    public final UserService userValidService;
 
     @Autowired
-    public UserValidator(UserValidService userValidService) {
+    public UserValidator(UserService userValidService) {
         this.userValidService = userValidService;
     }
 
